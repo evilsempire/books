a186efa507f824d1082ab480aaed37d98bd8d571
evilsempire <ashuomble5@gmail.com>
2019-12-17 17:32:57 +0530

    controller

---
+++ controllers/index.js
@@ -0,0 +1,121 @@
+const uuid4 = require('uuid')
+const { readFile, writeFile } = require('../datalayer')
+
+// get all the books
+const getBooks = (req, res, next) => {
+  readFile(
+    data => {
+      res
+        .status(200) // 200 success error
+        .send({
+          data,
+          status: 200,
+          message: null
+        })
+    },
+    true,
+    next
+  )
+}
+
+// get the book
+const getBook = (req, res, next) => {
+  readFile(
+    data => {
+      // now get the id
+      const id = req.params.id
+      let book
+      if (id) {
+        book = data.filter(item => item.id === id)
+        res.status(200).send({
+          status: 200,
+          data: book,
+          message: null
+        })
+      } else {
+        res.status(400).send({
+          status: 400,
+          data: null,
+          message: 'ID is required'
+        })
+      }
+    },
+    true,
+    next
+  )
+}
+
+// add the book
+const addBook = (req, res, next) => {
+  // id
+  readFile(
+    data => {
+      const id = uuid4()
+
+      // now create on object
+      const book = {
+        id,
+        name: req.body.name,
+        auther: req.body.auther,
+        content: req.body.content
+      }
+
+      // Now spread
+      const books = [...data, book]
+
+      writeFile(JSON.stringify(books), data => {
+        res.status(201).send({
+          status: 201,
+          message: 'Created Successfully'
+        })
+      })
+    },
+    true,
+    next
+  )
+}
+// delete the book
+const deleteBook = (req, res, next) => {
+  readFile(data => {
+    const id = req.body.id
+
+    if (typeof id !== 'undefined') {
+      // delete books now
+      const books = data.filter(item => item.id !== id)
+
+      if (books.length === data.length) {
+        res.status(400).send({
+          status: 400,
+          message: 'ID is not present in data',
+          data: []
+        })
+        res.end()
+      } else {
+        writeFile(
+          JSON.stringify(books),
+          () => {
+            res.status(200).send({
+              status: 200,
+              message: 'successfully deleted',
+              data: []
+            })
+          },
+          next
+        )
+      }
+    } else {
+      res.status(404).send({
+        message: 'No id specified',
+        data: [],
+        status: 400
+      })
+    }
+  }, true)
+}
+
+module.exports = {
+  getBooks,
+  getBook,
+  addBook,
+  deleteBook
+}
--- routes/books.js
+++ routes/books.js
@@ -1,118 +1,15 @@
-const uuid4 = require('uuid')
-const { readFile, writeFile } = require('../datalayer')
-
 const books = app => {
+  const books = require('../controllers')
   // READ ALL BOOKS
-  app.get('/books', (req, res, next) => {
-    readFile(
-      data => {
-        res
-          .status(200) // 200 success error
-          .send({
-            data,
-            status: 200,
-            message: null
-          })
-      },
-      true,
-      next
-    )
-  })
+  app.get('/books', books.getBooks)
 
   // GET ONLY ONE SINGLE BOOK
-  app.get('/book/:id?', (req, res, next) => {
-    readFile(
-      data => {
-        // now get the id
-        const id = req.params.id
-        let book
-        if (id) {
-          book = data.filter(item => item.id === id)
-          res.status(200).send({
-            status: 200,
-            data: book,
-            message: null
-          })
-        } else {
-          res.status(400).send({
-            status: 400,
-            data: null,
-            message: 'ID is required'
-          })
-        }
-      },
-      true,
-      next
-    )
-  })
+  app.get('/book/:id?', books.getBook)
 
   // add book
-  app.post('/post', (req, res, next) => {
-    // id
-    readFile(
-      data => {
-        const id = uuid4()
+  app.post('/post', books.addBook)
 
-        // now create on object
-        const book = {
-          id,
-          name: req.body.name,
-          auther: req.body.auther,
-          content: req.body.content
-        }
-
-        // Now spread
-        const books = [...data, book]
-
-        writeFile(JSON.stringify(books), data => {
-          res.status(201).send({
-            status: 201,
-            message: 'Created Successfully'
-          })
-        })
-      },
-      true,
-      next
-    )
-  })
-
-  app.delete('/delete/:id?', (req, res, next) => {
-    readFile(data => {
-      const id = req.body.id
-
-      if (typeof id !== 'undefined') {
-        // delete books now
-        const books = data.filter(item => item.id !== id)
-
-        if (books.length === data.length) {
-          res.status(400).send({
-            status: 400,
-            message: 'ID is not present in data',
-            data: []
-          })
-          res.end()
-        } else {
-          writeFile(
-            JSON.stringify(books),
-            () => {
-              res.status(200).send({
-                status: 200,
-                message: 'successfully deleted',
-                data: []
-              })
-            },
-            next
-          )
-        }
-      } else {
-        res.status(404).send({
-          message: 'No id specified',
-          data: [],
-          status: 400
-        })
-      }
-    }, true)
-  })
+  app.delete('/delete/:id?', books.deleteBook)
 }
 
 module.exports = books
